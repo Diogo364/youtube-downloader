6a6721adc65aeb8acc88168f226a9aeeb7143f16
Diogo364 <diogotnascimento94@gmail.com>
2024-04-21 12:20:44 -0300

    Add support to playlist download

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ if __name__ == '__main__':
     ap.add_argument('-o', '--output', default='out', type=str, help='Specify the output directory for downloaded videos.')
     ap.add_argument('-i', '--input', type=str, default=None, help='Path to an input file containing a list of video URLs.')
     ap.add_argument('--audio', action='store_true', default=False, help='Download only audio from video (mp3).')
-    ap.add_argument('url', default=None, nargs='*', help='One or more video URLs to download. You can provide multiple URLs separated by spaces.')
+    ap.add_argument('--url', default=None, nargs='*', help='One or more video URLs to download. You can provide multiple URLs separated by spaces.')
+    ap.add_argument('--playlist', action='store_true', help='Flag to treat URL as a Playlist to download entire content.')
     parser = ap.parse_args()
+    print(parser)
 
     assert any([parser.url, parser.input]), 'Please provide either an Input File, or a url.'
 
@@ -16,7 +18,7 @@ if __name__ == '__main__':
 
     stream_type = DownloadTypes.AUDIO if parser.audio else DownloadTypes.VIDEO
 
-    yd = YoutubeDownloader(stream_type=stream_type)
+    yd = YoutubeDownloader(stream_type=stream_type, playlist=parser.playlist)
 
     if not parser.input is None:
         with open(parser.input, 'r') as in_file:
--- models/youtube_downloader.py
+++ models/youtube_downloader.py
@@ -1,6 +1,6 @@
 import os.path as osp
 from enum import Enum
-from pytube import YouTube
+from pytube import YouTube, Playlist
 
 class DownloadTypes(Enum):
     VIDEO='.mp4'
@@ -10,11 +10,13 @@ class DownloadTypes(Enum):
         return {'type': self.name, 'extension': self.value}
 
 class YoutubeDownloader:
-    def __init__(self, url=None, stream_type=DownloadTypes.VIDEO):
+    def __init__(self, url=None, stream_type=DownloadTypes.VIDEO, playlist=False):
         self.url = url
-        self.video = None
+        self.videos = None
+        self.curr_video = None
         self.stream = None
         self.stream_type = stream_type
+        self.playlist = playlist
     
     @staticmethod
     def parse_media_options(media: DownloadTypes):
@@ -36,37 +38,40 @@ class YoutubeDownloader:
     def url(self, value):
         self.__url = value
 
-    def parse_filename_from_stream(self, extension='.mp4'):
-        return f'{self.video.title}{extension}'
+    def parse_filename_from_current_video(self, extension='.mp4'):
+        return f'{self.curr_video.title}{extension}'
 
     def set_output_path(self, out_root, extension):
-        return osp.join(out_root, self.parse_filename_from_stream(extension))
+        return osp.join(out_root, self.parse_filename_from_current_video(extension))
+    
+    def set_current_video(self, video):
+        self.curr_video = video
 
     def set_video(self):
-        self.video = YouTube(self.url)
+        self.videos = Playlist(self.url).videos if self.playlist else [YouTube(self.url)]
 
-    def get_video_stream(self):
-        self.stream = self.video.streams.get_highest_resolution()
+    def get_current_video_stream(self):
+        self.stream = self.curr_video.streams.get_highest_resolution()
     
-    def get_audio_stream(self):
-        self.stream = self.video.streams.get_audio_only()
+    def get_current_audio_stream(self):
+        self.stream = self.curr_video.streams.get_audio_only()
         
     def download(self, out_root: str):
-        try:
-            self.set_video()
-            print(f'Downloading {self.video.title}')
-            
-            out_path = self.set_output_path(out_root, self.stream_type['extension'])
-            if self.stream_type['type'] == 'VIDEO':
-                self.get_video_stream()
-            elif self.stream_type['type'] == 'AUDIO':
-                self.get_audio_stream()
-            else:
-                raise Exception(f"No implementation for {self.stream_type['type']}")
-            
-            self.stream.download(filename=out_path)
-            print('Downloaded')
-        except Exception as e:
-            print(f'An error ocurred: {e}')
-
-
+        self.set_video()
+        for video in self.videos:
+            try:
+                self.set_current_video(video)
+                print(f'Downloading {self.curr_video.title}')
+                
+                out_path = self.set_output_path(out_root, self.stream_type['extension'])
+                if self.stream_type['type'] == 'VIDEO':
+                    self.get_current_video_stream()
+                elif self.stream_type['type'] == 'AUDIO':
+                    self.get_current_audio_stream()
+                else:
+                    raise Exception(f"No implementation for {self.stream_type['type']}")
+                
+                self.stream.download(filename=out_path)
+                print('Downloaded')
+            except Exception as e:
+                print(f'An error ocurred: {e}')
